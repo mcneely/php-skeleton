b7d9f4c0f330bf73469f813a166334c9490273b4
Paul L. McNeely <paul@paulmcneely.com>
2020-05-01 07:03:51 -0500

    Moved nano to cli Correct port mix up.

--- .docker/php/cli/entrypoint.sh
+++ .docker/php/cli/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 APP_ENV=${APP_ENV:-prod}
-
-usermod -d /var/www root
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-curl -sSk https://getcomposer.org/installer | php -- --disable-tls && mv composer.phar /usr/local/bin/composer
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install -y --no-install-recommends --no-install-suggests supervisor nodejs yarn
-
+rm ~/.bashrc
+ln -s /var/www/.bashrc ~/.bashrc
 if [[ ! "dev" == "$APP_ENV" ]]; then
     if [[ -e /usr/local/etc/php/conf.d/xdebug.ini ]]; then
         rm -f /usr/local/etc/php/conf.d/xdebug.ini
@@ -20,5 +14,5 @@ else
     composer install
     composer container-deploy-dev
 fi
-
+touch /var/log/supervisor/supervisord.log
 /usr/bin/supervisord
